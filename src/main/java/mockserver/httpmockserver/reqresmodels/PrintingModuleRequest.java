3da62271d3f6ad038ca45628104803efb83fcb0c
Sergii Kukhar <skuhpass@gmail.com>
2019-10-12 21:00:15 +0300

    Update PrintingModule objects. Fix PrintingModule mocks

--- src/main/java/mockserver/httpmockserver/reqresmodels/PrintingModuleRequest.java
+++ src/main/java/mockserver/httpmockserver/reqresmodels/PrintingModuleRequest.java
@@ -3,15 +3,15 @@ package mockserver.httpmockserver.reqresmodels;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
-import java.io.Serializable;
+import ua.aval.integration.microservices.CoreBankSystemsRequest;
 
 import javax.json.bind.annotation.JsonbProperty;
 import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class PrintingModuleRequest implements Serializable {
+public class PrintingModuleRequest implements Serializable, CoreBankSystemsRequest {
 
     @JsonProperty("PI_CARD_ZAYAVA")
     @JsonbProperty("PI_CARD_ZAYAVA")
@@ -176,12 +176,17 @@ public class PrintingModuleRequest implements Serializable {
     @JsonbProperty("Dateopenacc")
     @NotNull(message = "Dateopenacc field in PrintingModuleRequest object connot be null.")
     private String Dateopenacc;
+    
+    @JsonProperty("operType")
+    @JsonbProperty("operType")
+    private String operType;
 
     /**
 	 * 
 	 */
 	public PrintingModuleRequest() {
 	}
+
 
     public String getPiCardZayava() {
         return piCardZayava;
@@ -454,4 +459,296 @@ public class PrintingModuleRequest implements Serializable {
     public void setDateopenacc(String dateopenacc) {
         Dateopenacc = dateopenacc;
     }
+    
+	public String getOperType() {
+		return operType;
+	}
+
+	public void setOperType(String operType) {
+		this.operType = operType;
+	}
+
+	@Override
+	public void requestOperationType(String operationType) {
+		this.operType = operationType;
+	}
+
+	@Override
+	public String fetchRequestOperationType() {
+		return this.operType;
+	}
+
+	@Override
+	public String fetchSourceSystemCode() {
+		return "";
+	}
+
+	@Override
+	public String fetchSourceSystemProcessId() {
+		return "";
+	}
+
+	@Override
+	public String fetchUserCredentials() {
+		return "";
+	}
+
+	@Override
+	public String fetchJwtToken() {
+		return "";
+	}
+
+
+	public static class Builder {
+
+        private String piCardZayava;
+        private String peCardZayava;
+        private String peCardDovidka;
+        private String ParamsFormat;
+        private String TemplateCode;
+        private String Output;
+        private String OPERATION;
+        private String customerCmdID;
+        private String productID;
+        private String typeId;
+        private String name;
+        private String surname;
+        private String secondname;
+        private String cardHolder;
+        private String taxcode;
+        private String birthDate;
+        private String docSer;
+        private String docNum;
+        private String publicAgreementNumber;
+        private String publicAgreementDate;
+        private String info_MOB_IB;
+        private String info_WORK;
+        private String info_TEL;
+        private String info_MOB_MB;
+        private String info_MOB;
+        private String email;
+        private String category;
+        private String cardName;
+        private String productName;
+        private String currency;
+        private String selectedProductId;
+        private String keyWord;
+        private String IBAN;
+        private String Dateopenacc;
+
+        public Builder () {}
+
+        public Builder piCardZayava (String val) {
+            piCardZayava = val;
+            return this;
+        }
+
+        public Builder peCardZayava (String val) {
+            peCardZayava = val;
+            return this;
+        }
+
+        public Builder peCardDovidka (String val) {
+            peCardDovidka = val;
+            return this;
+        }
+
+        public Builder ParamsFormat (String val) {
+            ParamsFormat = val;
+            return this;
+        }
+
+        public Builder TemplateCode (String val) {
+            TemplateCode = val;
+            return this;
+        }
+
+        public Builder Output (String val) {
+            Output = val;
+            return this;
+        }
+
+        public Builder OPERATION (String val) {
+            OPERATION = val;
+            return this;
+        }
+
+        public Builder customerCmdID (String val) {
+            customerCmdID = val;
+            return this;
+        }
+
+        public Builder productID (String val) {
+            productID = val;
+            return this;
+        }
+
+        public Builder typeId (String val) {
+            typeId = val;
+            return this;
+        }
+
+        public Builder name (String val) {
+            name = val;
+            return this;
+        }
+
+        public Builder surname (String val) {
+            surname = val;
+            return this;
+        }
+
+        public Builder secondname (String val) {
+            secondname = val;
+            return this;
+        }
+
+        public Builder cardHolder (String val) {
+            cardHolder = val;
+            return this;
+        }
+
+        public Builder taxcode (String val) {
+            taxcode = val;
+            return this;
+        }
+
+        public Builder birthDate (String val) {
+            birthDate = val;
+            return this;
+        }
+
+        public Builder docSer (String val) {
+            docSer = val;
+            return this;
+        }
+
+        public Builder docNum (String val) {
+            docNum = val;
+            return this;
+        }
+
+        public Builder publicAgreementNumber (String val) {
+            publicAgreementNumber = val;
+            return this;
+        }
+
+        public Builder publicAgreementDate (String val) {
+            publicAgreementDate = val;
+            return this;
+        }
+
+        public Builder info_MOB_IB (String val) {
+            info_MOB_IB = val;
+            return this;
+        }
+
+        public Builder info_WORK (String val) {
+            info_WORK = val;
+            return this;
+        }
+
+        public Builder info_TEL (String val) {
+            info_TEL = val;
+            return this;
+        }
+
+        public Builder info_MOB_MB (String val) {
+            info_MOB_MB = val;
+            return this;
+        }
+
+        public Builder info_MOB (String val) {
+            info_MOB = val;
+            return this;
+        }
+
+        public Builder email (String val) {
+            email = val;
+            return this;
+        }
+
+        public Builder category (String val) {
+            category = val;
+            return this;
+        }
+
+        public Builder cardName (String val) {
+            cardName = val;
+            return this;
+        }
+
+        public Builder productName (String val) {
+            productName = val;
+            return this;
+        }
+
+        public Builder currency (String val) {
+            currency = val;
+            return this;
+        }
+
+        public Builder selectedProductId (String val) {
+            selectedProductId = val;
+            return this;
+        }
+
+        public Builder keyWord (String val) {
+            keyWord = val;
+            return this;
+        }
+
+        public Builder IBAN (String val) {
+            IBAN = val;
+            return this;
+        }
+
+        public Builder Dateopenacc (String val) {
+            Dateopenacc = val;
+            return this;
+        }
+
+        public PrintingModuleRequest build() {
+            return new PrintingModuleRequest(this);
+        }
+
+    }
+
+    private PrintingModuleRequest (Builder builder) {
+        piCardZayava = builder.piCardZayava;
+        peCardZayava = builder.peCardZayava;
+        peCardDovidka = builder.peCardDovidka;
+        ParamsFormat = builder.ParamsFormat;
+        TemplateCode = builder.TemplateCode;
+        Output = builder.Output;
+        OPERATION = builder.OPERATION;
+        customerCmdID = builder.customerCmdID;
+        productID = builder.productID;
+        typeId = builder.typeId;
+        name = builder.name;
+        surname = builder.surname;
+        secondname = builder.secondname;
+        cardHolder = builder.cardHolder;
+        taxcode = builder.taxcode;
+        birthDate = builder.birthDate;
+        docSer = builder.docSer;
+        docNum = builder.docNum;
+        publicAgreementNumber = builder.publicAgreementNumber;
+        publicAgreementDate = builder.publicAgreementDate;
+        info_MOB_IB = builder.info_MOB_IB;
+        info_WORK = builder.info_WORK;
+        info_TEL = builder.info_TEL;
+        info_MOB_MB = builder.info_MOB_MB;
+        info_MOB = builder.info_MOB;
+        email = builder.email;
+        category = builder.category;
+        cardName = builder.cardName;
+        productName = builder.productName;
+        currency = builder.currency;
+        selectedProductId = builder.selectedProductId;
+        keyWord = builder.keyWord;
+        IBAN = builder.IBAN;
+        Dateopenacc = builder.Dateopenacc;
+    }
+
 }
--- src/main/java/mockserver/httpmockserver/reqresmodels/PrintingModuleResponse.java
+++ src/main/java/mockserver/httpmockserver/reqresmodels/PrintingModuleResponse.java
@@ -2,26 +2,38 @@ package mockserver.httpmockserver.reqresmodels;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import ua.aval.integration.microservices.CoreBankSystemsResponse;
 
 import javax.json.bind.annotation.JsonbProperty;
 import java.io.Serializable;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class PrintingModuleResponse implements Serializable {
+public class PrintingModuleResponse implements Serializable, CoreBankSystemsResponse {
 
     @JsonProperty("url")
     @JsonbProperty("url")
     private String url;
     
+    @JsonProperty("error")
+    @JsonbProperty("error")
     private Error error;
 
     /**
-	 * 
+	 *
 	 */
 	public PrintingModuleResponse() {
 	}
-	public PrintingModuleResponse(String errCode) {
-		this.error = new Error(errCode, "No message");
+
+	@Override
+	public String fetchErrorCode() {
+		if (this.error == null) return null;
+		return this.error.getErrCode();
+	}
+
+	@Override
+	public String fetchErrorMessage() {
+		if (this.error == null) return null;
+		return this.error.getErrMes();
 	}
 
 	public String getUrl() {
@@ -31,9 +43,11 @@ public class PrintingModuleResponse implements Serializable {
     public void setUrl(String url) {
         this.url = url;
     }
+
 	public Error getError() {
 		return error;
 	}
+
 	public void setError(Error error) {
 		this.error = error;
 	}
